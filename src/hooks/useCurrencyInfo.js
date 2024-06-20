dcf99d3bf7d7b6b6fd27b4915d9590c6cb5218ae
Debasish Bhola <devop69@gmail.com>
2024-06-20 20:29:04 +0530

    Custom Hook added

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency){
     const [data,setData] = useState({}); 
 
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
         .then((res)=>res.json())
         .then((res)=>setData(res[currency]))
     },[currency])
@@ -13,4 +13,4 @@ function useCurrencyInfo(currency){
     return data;
 }
 
-export default useCurrencyInfo;
+export default useCurrencyInfo
